e126c906816e04daa5563c45c15f0b22b478fb55
Quang Si <tranquangsidev@gmail.com>
2021-06-08 22:41:05 +0700

    impliment sql

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  user: "root",
-  password: "antelopeSQL",
-  host: "localhost",
-  database: "todotutorial",
+  user: "ul9vumq2nudxfzdx",
+  password: "SBLnU49dMjEV6dasMyMs",
+  host: "bqzivh3ntgqaxu0vj4dy-mysql.services.clever-cloud.com",
+  database: "bqzivh3ntgqaxu0vj4dy",
 });
 module.exports = pool;
